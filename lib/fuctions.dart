8871f1ff3898a8e5b62640c2e0939874d507b455
santhosh <santhoshsi143@gmail.com>
2023-11-14 10:05:22 +0530

    first commit

---
+++ lib/fuctions.dart
@@ -0,0 +1,38 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:student_db/home_screen.dart';
+import 'package:student_db/student_model.dart';
+addStudent(StudentModel model) async {
+  final box = await Hive.openBox<StudentModel>('student_record');
+  studentListNotifier.value.add(model);
+  box.add(model);
+  studentListNotifier.notifyListeners();
+}
+
+updatestudent(
+    StudentModel oldStudent, StudentModel newStudent, int index) async {
+  final box = await Hive.openBox<StudentModel>('student_record');
+
+  studentListNotifier.value.remove(oldStudent);
+  studentListNotifier.value.insert(index, newStudent);
+  box.putAt(index, newStudent);
+  studentListNotifier.notifyListeners();
+}
+
+removeStudent(int index) async {
+  final box = await Hive.openBox<StudentModel>('student_record');
+  studentListNotifier.value.removeAt(index);
+  box.deleteAt(index);
+  studentListNotifier.notifyListeners();
+}
+
+getAllStudent() async {
+  final box = await Hive.openBox<StudentModel>('student_record');
+  studentListNotifier.value.clear();
+  studentListNotifier.value.addAll(box.values);
+ 
+  studentListNotifier.notifyListeners();
+}
+
+search() async {
+  final box = await Hive.openBox<StudentModel>('student_record');
+}
